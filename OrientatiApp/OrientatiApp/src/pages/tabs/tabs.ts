8aa0e7243b483c3f23c8d0a660f6cd4717e4c912
peppe itta <peppe.itta@gmail.com>
2017-04-13 17:52:12 +0200

    Theming Parziale (pt.1)

--- OrientatiApp/OrientatiApp/src/pages/tabs/tabs.ts
+++ OrientatiApp/OrientatiApp/src/pages/tabs/tabs.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 
 @Component({
+  selector: "page-tabs",
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
